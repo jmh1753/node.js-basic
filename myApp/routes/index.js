5452c93709ece358ce8f241ab0d055a8a8403342
jmh1753 <kz1324@naver.com>
2019-11-02 15:53:57 +0900

    first commit

---
+++ myApp/routes/index.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var router = express.Router();
+var mysql = require('mysql');
+
+let client = mysql.createConnection({
+  user: "root",
+  password: "wjdansgh1!",
+  database: "mysqltest"
+})
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/edit', function(req, res, next){
+  res.render('product/edit', {
+    title : "Express~"
+  });
+});
+
+router.get('/create', function(req, res, next){
+  client.query("select * from products", function(err, result, fields){
+    if(err){
+      console.log(err);
+      console.log("쿼리문에 오류가 있습니다.");
+    }else{
+      res.render('create', {
+        results : result
+      });
+    }
+  });
+});
+
+router.post('/create', function(req, res, next){
+  var body = req.body;
+
+  client.query("insert into products(name, modelnumber, series) values(?,?,?)", [
+    body.name, body.modelnumber, body.series
+  ], function(){
+    res.redirect("/create");
+  });         
+});
+
+module.exports = router;
